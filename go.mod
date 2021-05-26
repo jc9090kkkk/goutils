693f7495d9837e5139843547ea92721fe4675a3a
cjia <cjia1@i9i8.com>
2021-05-26 15:03:39 +0800

    'md5.go'

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jc9090kkkk/goutils
+
+go 1.16
---
+++ hash/md5.go
@@ -0,0 +1,13 @@
+package hash
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// get string md5
+func StringMd5(s string) string {
+	md5 := md5.New()
+	md5.Write([]byte(s))
+	return hex.EncodeToString(md5.Sum(nil))
+}
